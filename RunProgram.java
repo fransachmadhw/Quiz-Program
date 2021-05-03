be3a74033462c9580704c9e31fb6451c223dc229
Frans AHW <franswinata6@gmail.com>
2021-05-03 14:10:38 +0700

    First Commit

---
+++ RunProgram.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class RunProgram {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, good luck on working these questions", "Computer Science Quiz",
+                JOptionPane.INFORMATION_MESSAGE);
+        Starter frame = new Starter();
+        // JFrame obj = new JFrame();
+        // obj.setBounds(10, 10, 700, 600);
+        // obj.setTitle("Application Form");
+        // obj.setResizable(false);
+        // obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
